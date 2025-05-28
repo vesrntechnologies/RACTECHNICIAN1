90101ef853d7e8f926fb18020e88ecbd9ff9b74a
vesrntechnologies <vennjoy@gmail.com>
2025-05-28 15:56:13 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -2,18 +2,64 @@
 let questions = [
     {
     numb: 1,
-    question: "Which class of fire is caused by fire wood, paper or cloth?",
+    question: "Which extinguishes the fire of flammable liquids?",
   //  immg:"img1",
-    answer: "Class A fire",
+    answer: "CO2",
     options: [
-      "Class A fire",
-      "Class B fire",
-      "Class C Fire",
-      "Class D Fie"
+      "Water",
+      "Air",
+      "CO2",
+      "Oil"
     ]
   },
-    
-    
+      {
+    numb: 2,
+    question: "What is the type of chisel?",
+    immg:"img2",
+    answer: "Diamond Point",
+    options: [
+      "Flat",
+      "Cross Cut",
+      "Half round",
+      "Diamond Point"
+    ]
+  },
+     {
+    numb: 3,
+    question: "Which materials are used to make mallets?",
+   
+    answer: "Wood, rubber",
+    options: [
+      "Wood, cast iron",
+      "Wood, rubber",
+      "Rubber, brass",
+      "Wood, brass"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is tje angle marked as X in sheet metal V notch?",
+    immg:"img4",
+    answer: "90o",
+    options: [
+      "90o",
+      "60o",
+      "45o",
+      "30o"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which tool is used for bending, seaming and forming of sheet metal?",
+    //immg:"img2",
+    answer: "Stakes",
+    options: [
+      "Bench vice",
+      "Hand Vice",
+      "Stakes",
+      "Shear"
+    ]
+  },
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
