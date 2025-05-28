f6d7391bfe93560c48fcbd8cbd20532bc23e808a
vesrntechnologies <vennjoy@gmail.com>
2025-05-28 15:13:03 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,31 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which class of fire is caused by fire wood, paper or cloth?",
+  //  immg:"img1",
+    answer: "Class A fire",
+    options: [
+      "Class A fire",
+      "Class B fire",
+      "Class C Fire",
+      "Class D Fie"
+    ]
+  },
+    
+    
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
